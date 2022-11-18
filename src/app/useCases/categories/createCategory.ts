81ebfce70337a0704da77ca651bdbc5bf0b3af29
Bia Gavira <biagavirete@gmail.com>
2022-11-17 21:46:48 -0300

    feat: complete routes for products, categories and orders

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -2,9 +2,22 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function createCategory(req: Request, res: Response) {
-  const { icon, name } = req.body;
+  try {
+    const { icon, name } = req.body;
 
-  const category = await Category.create({ icon, name });
+    if (!icon || !name) {
+      res.status(400).json({
+        error: 'Name and icon are required'
+      });
+    }
 
-  res.json(category);
+    const category = await Category.create({ icon, name });
+
+    res.status(201).json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: 'Internal Server Error'
+    });
+  }
 }
